245701e2099355d902ac654033f9fdabeac91988
nao-sato <72682955+nao-sato@users.noreply.github.com>
2021-01-21 00:18:40 +0900

    ・ViewPager２を改良

--- app/src/main/java/com/example/ktAndroidSample/viewPager2/SampleFragment3.kt
+++ app/src/main/java/com/example/ktAndroidSample/viewPager2/SampleFragment3.kt
@@ -1,17 +1,14 @@
 package com.example.ktAndroidSample.viewPager2
 
-import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.example.ktAndroidSample.R
 import com.example.ktAndroidSample.databinding.FragmentSample1Binding
 
-
-class SampleFragment1 : Fragment() {
+class SampleFragment3 : Fragment() {
 
     lateinit var binding: FragmentSample1Binding
 
@@ -19,10 +16,10 @@ class SampleFragment1 : Fragment() {
     ): View {
         binding = FragmentSample1Binding.inflate(inflater,container,false)
         binding.apply {
-            word.setText(R.string.F1)
+            word.setText(R.string.F3)
             content.setBackgroundResource(R.color.CornflowerBlue)
         }
-
         return binding.root
+
     }
 }
